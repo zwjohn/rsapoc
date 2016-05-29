4f2d42279a6e91316725584e0f5c017cc7497be3
zwjohn <zwjohn@yahoo.com>
2016-05-29 13:30:18 +0000

    Add files via upload

---
+++ server.js
@@ -0,0 +1,48 @@
+var express = require('express'),
+    request = require('request'),
+    bodyParser = require('body-parser'),
+    app = express();
+
+var myLimit = typeof(process.argv[2]) != 'undefined' ? process.argv[2] : '10000kb';
+var myPort = 3333;
+console.log('Using limit: ', myLimit);
+var urlPrefix = 'http://localhost:'+myPort+'/';
+
+app.use(bodyParser.json({limit: myLimit}));
+
+app.all('*', function (req, res, next) {
+
+    // Set CORS headers: allow all origins, methods, and headers: you may want to lock this down in a production environment
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET, PUT, PATCH, POST, DELETE");
+    res.header("Access-Control-Allow-Headers", req.header('access-control-request-headers'));
+
+    if (req.method === 'OPTIONS') {
+        // CORS Preflight
+        res.send();
+    } else {
+
+        var targetURL = req.url.substring(1,req.url.length);
+
+        if (!targetURL) {
+            res.send(500, { error: 'There is no Target-Endpoint header in the request' });
+            return;
+        }
+        console.log(targetURL );
+        console.log(req.method);
+        console.log(req.header('Authorization'));
+        request({ url: targetURL, method: req.method, json: req.body, headers: {'Authorization': req.header('Authorization')} },
+            function (error, response, body) {
+                if (error) {
+                    console.error('error: ' + response.statusCode)
+                }
+//                console.log(body);
+            }).pipe(res);
+    }
+});
+
+app.set('port', myPort);
+
+app.listen(app.get('port'), function () {
+    console.log('Proxy server listening on port ' + app.get('port'));
+});
